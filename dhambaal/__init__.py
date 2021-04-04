e30c20707c8730a48d63b0735ec12966b3ef1786
Hibo Abdi <hibohonest11@gmail.com>
2021-04-04 11:39:01 +0300

    updated

--- dhambaal/__init__.py
+++ dhambaal/__init__.py
@@ -2,20 +2,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_migrate import Migrate
 from flask_login import LoginManager
-#from flask-bcrypt import 
 from flask_bcrypt import Bcrypt
-
+from flask_mail import Mail
+from dhambaal.config import DevelopmentConfig
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "thisisverysecure"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dhambaal.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SECRET_KEY'] = 'thisisverysuecure'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dhambaal.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# TODO1 : The application should automatically load configurations
+app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+mail = Mail(app)
 
+print(app.config)
 # Create Tables before running application
+
+
 @app.before_first_request
 def create_tables():
     db.create_all()
